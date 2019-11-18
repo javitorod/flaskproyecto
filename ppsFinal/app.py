343621d1c20245b9cf0fe61691b19e10126ea3df
mdilauro39 <mdiauro39@gmail>
2019-11-18 15:17:16 -0300

    subiendo app inicial

--- ppsFinal/app.py
+++ ppsFinal/app.py
@@ -1,43 +1,28 @@
-from flask import Flask, request, flash, url_for, redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Response
+import json
+#importamos el jsoni
+from flask import jsonify
+#importamos flask
+from flask import Flask
+#iniciamos una app 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
-app.config['SECRET_KEY'] = "random string"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+#Creamos una ruta de hola mundo
+@app.route("/")
+def hello_World():
+    return  "Hello, World!"
 
-class User(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column('id', db.Integer, primary_key = True)
-    name = db.Column(db.String(100))
-    password = db.Column(db.String(50))
-
-
-    def __init__(self,name,password):
-        self.name = name
-        self.password = password
-  
- 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route('/hello/<name>')   
-def hello(name):
-    return 'welcome %s' % name
-
-@app.route('/fluxer', methods=['GET'])
+#Creamos una ruta con el metodo GET
+@app.route("/fluxer", methods=['get'])
 def fluxer_index():
-    return('fluxer_index')
+    return('fluxer index')
 
-@app.route('/fluxer', methods=['POST'])
+#creamos una ruta con el metodo POST
+@app.route('/fluxer', methods=['post'])
 def fluxer_create():
-    return('fluxer_create')
+    data = [{"message":"fluxer_created"}]
+    #return('fluxer created')
+    return Response(json.dumps(data), mimetype='application/json')
 
 if __name__ == '__main__':
-   db.create_all()
-   app.run(debug = True)
+    app.run()
